d87991c75ab06feb80f13ad6e639ade7187e5e38
weihaiwh <595585742@qq.com>
2024-07-06 11:49:05 +0800

    Update game.h
    zzz

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.miHoYo.Nap"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
